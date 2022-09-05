0037314f088d806c97ff5aa285d1ea4cd8615fd9
OTabelini <otabelini@gmail.com>
2022-09-04 21:17:48 -0300

    Start

---
+++ README.txt
@@ -0,0 +1,8 @@
+ABOUT THE BOT
+
+It's a telegram bot, made for provide career information (focused in data analytics). 
+With this bot, the HR can access the bot to know you better, download your cv and other basic informations.
+In the bot_telegram file you'll find the code documented to guide you to change whatever you want to make this tool worth it for you.
+
+PIP INSTALL 
+To make the bot run, you have to pip install pytelegrambotapi on your terminal 
---
+++ bot_telegram.py
@@ -0,0 +1,93 @@
+import telebot
+
+
+# You can get this key from @BotFather on Telegram
+api_key = "xxxxxxxxxxxxx"
+
+# Here is the command that the bot are created 
+bot = telebot.TeleBot(api_key)
+
+# Always put message_handler and a option together.
+@bot.message_handler(commands=["option1"])
+def option1(message):
+    bot.send_message(message.chat.id, 
+    """
+RESUME ABOUT YOUR EDUCATION
+    """
+    )
+
+@bot.message_handler(commands=["option2"])
+def option2(message):
+    bot.send_message(message.chat.id, 
+    """
+SUMMARY OF SKILLS!!!!!!!!!!
+
+> aaaaaaaa;
+> bbbbbbbb;
+> cccccccc;
+> dddddddd;
+> eeeeeeee;
+> ffffffff;
+> gggggggg;
+
+etc.
+    """
+    )
+
+@bot.message_handler(commands=["option3"])
+def option3(message):
+    bot.send_message(message.chat.id, "Age and localization")
+
+@bot.message_handler(commands=["option4"])
+def option4(message):
+    bot.send_message(message.chat.id,
+    """
+LinkedIn:
+Email:
+Cel:
+    """
+    )
+
+@bot.message_handler(commands=["option5"])
+def option5(message):
+    # print(message)
+    bot.send_message(message.chat.id, 
+    """
+    > Link: https://github.com/USER
+    """
+    )
+
+@bot.message_handler(commands=["option6"])
+def option6(message):
+    # print(message)
+    bot.send_message(message.chat.id, 
+    """
+You can put here the link from (drive or any cloud storage) your cv.
+    """
+    )
+
+def check(message):
+    return True
+
+@bot.message_handler(func=check)
+def reply(message):
+    text = f"""
+    Hello {message.from_user.first_name}, welcome!
+    I'm bot XXXXXXX!
+    
+    Choose one option to continue (click in one of them):
+
+    /option1 Education               (EXAMPLE)
+    /option2 Summary of Skills       (EXAMPLE)
+    /option3 Age and Localization    (EXAMPLE)
+    /option4 Contact                 (EXAMPLE)
+    /option5 GitHub                  (EXAMPLE)
+    /option6 Resumee - CV            (EXAMPLE)
+    
+Please, select one option above to proceed the information.
+ 
+"""
+    bot.send_message(message.chat.id, text) 
+
+# This function are responsable to make the bot responsivem check that and other functions that I use here in the documentation of telebot
+bot.polling()
